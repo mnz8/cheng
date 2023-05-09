32fb44988af5c3200ee0e3855153adefc7932ea7
mnz8 <mnz8@foxmail.com>
2023-05-09 23:40:17 +0800

    feat: nan-tian-men

--- back/nan-tian-men/src/app.controller.ts
+++ back/nan-tian-men/src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { AUTHENTICATION_SERVICE, AUTHENTICATION_MP, USER_MANAGER_SERVICE, USER_MANAGER_MP, BLOG_SERVICE } from '../../constants';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(AUTHENTICATION_SERVICE) private authentication_client: ClientProxy,
+    @Inject(USER_MANAGER_SERVICE) private user_manager_client: ClientProxy,
+    @Inject(BLOG_SERVICE) private blog_client: ClientProxy,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('authentication')
+  authentication(): Observable<string> {
+    return this.authentication_client.send(AUTHENTICATION_MP, 'nan-tian-men send to authentication_service');
+  }
+
+  @Get('user-manager')
+  user_manager(): Observable<string> {
+    return this.user_manager_client.send(USER_MANAGER_MP, 'nan-tian-men send to user_manager_service');
   }
 }
--- back/nan-tian-men/src/app.module.ts
+++ back/nan-tian-men/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { AUTHENTICATION_SERVICE, AUTHENTICATION_I_PORT, USER_MANAGER_SERVICE, USER_MANAGER_I_PORT } from '../../constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: AUTHENTICATION_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          port: AUTHENTICATION_I_PORT,
+        },
+      },
+    ]),
+    ClientsModule.register([
+      {
+        name: USER_MANAGER_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          port: USER_MANAGER_I_PORT,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
