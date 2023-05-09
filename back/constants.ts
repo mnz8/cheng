e1d29544a413dfb85777081e8b55416faccef2fa
line126 <line112266@126.com>
2023-05-09 23:28:17 +0800

    feat: authentication

---
+++ back/constants.ts
@@ -0,0 +1,14 @@
+// ClientsModule name
+export const AUTHENTICATION_SERVICE = "authentication_service";
+export const USER_MANAGER_SERVICE = "user_manager_service";
+export const BLOG_SERVICE = "blog_service";
+// Controller MessagePattern
+export const AUTHENTICATION_MP = "authentication_mp";
+export const USER_MANAGER_MP = "user_manager_mp";
+// port
+export const AUTHENTICATION_O_PORT = 60002;
+export const AUTHENTICATION_I_PORT = 59998;
+export const USER_MANAGER_I_PORT = 59999;
+export const BLOG_I_PORT = 50001;
+
+export const NAN_TIAN_MEN_O_PORT = 60001;
---
+++ port.md
@@ -0,0 +1,11 @@
+internal: 5xxxx
+outernal: 6xxxx
+
+| port         | project        | type          |
+| ------------ | -------------- | ------------- |
+| 60002, 59998 | authentication | http + tcp    |
+| 60003, 59997 | authorization  | http + tcp    |
+| xxxxx, 50001 | blog           | rpc           |
+| 60004        | gateway        | microservices |
+| 60001        | nan-tian-men   | microservices |
+| xxxxx, 59999 | user-manager   | tcp           |
