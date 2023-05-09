79bc175dbf61c84871cfcbdacfeb271f54b65f55
mnz8 <mnz8@foxmail.com>
2023-05-09 23:39:58 +0800

    feat: blog

--- back/blog/src/main.ts
+++ back/blog/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { BLOG_I_PORT } from '../../constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: `localhost:${BLOG_I_PORT}`,
+      package: 'blog',
+      // 路径跟 back\constants.ts 有关
+      protoPath: join(__dirname, '../../blog.proto'),
+    },
+  });
+  await app.listen();
 }
+
 bootstrap();
