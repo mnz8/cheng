a24d82c733c97215daf986b79ee63d281b84ba2f
line126 <line112266@126.com>
2023-05-10 00:06:09 +0800

    feat: blog

--- back/nan-tian-men/src/app.module.ts
+++ back/nan-tian-men/src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppService } from './app.service';
-import { AUTHENTICATION_SERVICE, AUTHENTICATION_I_PORT, USER_MANAGER_SERVICE, USER_MANAGER_I_PORT } from '../../constants';
+import { AUTHENTICATION_SERVICE, AUTHENTICATION_I_PORT, USER_MANAGER_SERVICE, USER_MANAGER_I_PORT, BLOG_SERVICE, BLOG_I_PORT } from '../../constants';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -21,6 +22,18 @@ import { AUTHENTICATION_SERVICE, AUTHENTICATION_I_PORT, USER_MANAGER_SERVICE, US
         transport: Transport.TCP,
         options: {
           port: USER_MANAGER_I_PORT,
+        },
+      },
+    ]),
+    ClientsModule.register([
+      {
+        name: BLOG_SERVICE,
+        transport: Transport.GRPC,
+        options: {
+          url: `localhost:${BLOG_I_PORT}`,
+          package: 'blog',
+          // 路径跟 back\constants.ts 有关
+          protoPath: join(__dirname, '../../../../blog/src/blog.proto'),
         },
       },
     ]),
