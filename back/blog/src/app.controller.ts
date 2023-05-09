a9bdc380711629680c81e27291eba16792bb2d3d
line126 <line112266@126.com>
2023-05-09 23:39:58 +0800

    feat: blog

--- back/blog/src/app.controller.ts
+++ back/blog/src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
+import { Observable, Subject } from 'rxjs';
+import { BlogById } from './interfaces/blog-by-id.interface';
+import { Blog } from './interfaces/blog.interface';
 
-@Controller()
+@Controller('blog')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly items: Blog[] = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Doe' },
+  ];
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @GrpcMethod('BlogService')
+  findOne(data: BlogById): Blog {
+    return this.items.find(({ id }) => id === data.id);
+  }
+
+  @GrpcStreamMethod('BlogService')
+  findMany(data$: Observable<BlogById>): Observable<Blog> {
+    const blog$ = new Subject<Blog>();
+
+    const onNext = (blogById: BlogById) => {
+      const item = this.items.find(({ id }) => id === blogById.id);
+      blog$.next(item);
+    };
+    const onComplete = () => blog$.complete();
+    data$.subscribe({
+      next: onNext,
+      complete: onComplete,
+    });
+
+    return blog$.asObservable();
   }
 }
