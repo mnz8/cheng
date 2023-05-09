cf8c64af1bf56e768f54037729c980e7b797257a
line126 <line112266@126.com>
2023-05-09 23:39:11 +0800

    feat: gateway port

--- back/gateway/src/main.ts
+++ back/gateway/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GATEWAY_O_PORT } from '../../constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(GATEWAY_O_PORT);
 }
 bootstrap();
