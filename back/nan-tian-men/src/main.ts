9de6823c96d171d6807c69a9dd908a99070f71af
line126 <line112266@126.com>
2023-05-09 23:40:17 +0800

    feat: nan-tian-men

--- back/nan-tian-men/src/main.ts
+++ back/nan-tian-men/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NAN_TIAN_MEN_O_PORT } from '../../constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 设置统一前缀
+  app.setGlobalPrefix('/nan-tian-men');
+  await app.listen(NAN_TIAN_MEN_O_PORT);
 }
 bootstrap();
