69f6cc97fc901fcbeddec9493bdce8fb88e2cfd6
line126 <line112266@126.com>
2023-05-09 23:38:12 +0800

    feat: authorization port

--- back/constants.ts
+++ back/constants.ts
@@ -10,5 +10,8 @@ export const AUTHENTICATION_O_PORT = 60002;
 export const AUTHENTICATION_I_PORT = 59998;
 export const USER_MANAGER_I_PORT = 59999;
 export const BLOG_I_PORT = 50001;
+export const AUTHORIZATION_O_PORT = 60003;
+export const AUTHORIZATION_I_PORT = 59997;
+export const GATEWAY_O_PORT = 60004;
 
 export const NAN_TIAN_MEN_O_PORT = 60001;
